00a4d5eee0940930b371892182e63ffedcaf246b
KamaljeetSahoo <kamaljeetsahoo@gmail.com>
2022-09-18 09:56:27 +0530

    Frontend pages added

--- prescription/urls.py
+++ prescription/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,uploadPrescription,viewPrescription,Prescriptions,Dashboard
 
 urlpatterns = [
     path('', homepage, name="home"),
+    path('uploadPrescription/',uploadPrescription,name = 'upload'),
+    path('viewPrescription/',viewPrescription,name = 'viewPrescription'),
+    path('prescriptions/',Prescriptions,name = 'Prescriptions'),
+    path('dashboard/',Dashboard, name = 'Dashboard')
 ]
--- prescription/views.py
+++ prescription/views.py
@@ -6,3 +6,32 @@ def homepage(request):
         return render(request, 'pages/homepage.html')
     else:
         return redirect('/login')
+
+
+def uploadPrescription(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'pages/uploadPrescription.html')
+    else:
+        return redirect('login')
+
+def viewPrescription(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'pages/viewPrescription.html')
+    else:
+        return redirect('login')
+
+def Prescriptions(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'pages/prescriptions.html')
+    else:
+        return redirect('login')
+
+def Dashboard(request):
+
+    if request.user.is_authenticated:
+        return render(request, 'pages/dashboard.html')
+    else:
+        return redirect('login')
