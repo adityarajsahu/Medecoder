df3b576ec0e624ed283477679c6eaec5f74444d1
Sanimesh96 <sanimesh96@gmail.com>
2023-03-02 14:43:04 +0530

    Static File URL changed

--- MEDECODER/settings.py
+++ MEDECODER/settings.py
@@ -122,7 +122,6 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
